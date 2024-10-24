a56c20d0a6541c74255d2549a7c604c67e3d974f
Sebastien Castiel <sebastien@castiel.me>
2024-10-23 21:06:41 -0400

    I must get out of these wet clothes and into a dry Martini. 		-- Alexander Woolcott

--- init.lua
+++ init.lua
@@ -502,6 +502,9 @@ require("lazy").setup({
 							},
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 							-- diagnostics = { disable = { 'missing-fields' } },
+							diagnostics = {
+								globals = { "vim" },
+							},
 						},
 					},
 				},
