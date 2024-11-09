6376751a0a4e2db78f202114253397cebda4451b
Sebastien Castiel <sebastien@castiel.me>
2024-11-09 14:40:10 -0500

    Acting is an art which consists of keeping the audience from coughing.

--- init.lua
+++ init.lua
@@ -774,10 +774,19 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 
